f1f8d75bde29370ea5f7b3156aacfecb03ec267c
norma-forson <norma.forson@ashesi.edu.gh>
2023-12-19 23:24:05 -0500

    First Commit

---
+++ First.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Norma
+{
+    public:
+    string name;
+    int age;
+    Norma(string Name, int Age)
+    {
+        name = Name;
+        age = Age;
+    }
+
+    virtual void echoDeets()
+    {
+        cout<<"s'up"<<endl;
+    }
+};
+
+class subNorm : public Norma
+{
+    string middleName;
+    public:
+    subNorm(string Name, int Age, string midName): Norma(Name, Age)
+    {
+        middleName = midName;
+    }
+    void echoDeets()
+    {
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<middleName<<endl;
+    }
+};
+
+
+int main()
+{
+    subNorm Norms("Norma", 22, "NanaYaa");
+    Norma* baseClass = &Norms;
+
+    baseClass->echoDeets();
+
+
+    return 0;
+
+}
